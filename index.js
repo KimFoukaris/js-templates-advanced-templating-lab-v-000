e2c554550bfe74ce098d60b586567b2c6ba6ed34
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-15 19:53:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
 
-  Handlebars.registerHelper('displayIngredient', function(value) {
+  Handlebars.registerHelper('displayIngredient', function(event) {
     return new Handlebars.SafeString(
-        '<li>' + value + '</li>');
+        '<li>' + event + '</li>');
     });
 
 }
