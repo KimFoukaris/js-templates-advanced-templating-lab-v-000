d28c2b341d25c88f9ac240c38f5262dc38c19b63
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-15 19:51:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+  Handlebars.registerHelper('displayIngredient', function(value) {
+    return new Handlebars.SafeString(
+        '<li>' + value + '</li>');
+    });
+
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+  init();
+  displayForm()
 })
+
+function displayForm() {
+  var recipeForm = document.getElementById("recipe-form")
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  recipeForm.innerHTML = template()
+}
+
+function displayEditForm() {
+  var recipeForm = document.getElementById("recipe-form")
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var html = template(recipe);
+  editForm.innerHTML = html;
+}
+
+function createRecipe() {
+  var recipe = {
+    name: document.getElementById("name").value,
+    description: document.getElementById("description").value
+
+  }
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+  var html = template(recipe)
+  document.getElementsByTagName("main").innerHTML;
+}
+
+function editRecipe() {
+
+}
